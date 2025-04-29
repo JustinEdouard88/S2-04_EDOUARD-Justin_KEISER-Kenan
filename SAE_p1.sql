434f4950e71cac5395d43207ecdd4b89a06458a4
Kenan KEISER <kenan.keiser2@etu.univ-lorraine.fr>
2025-04-29 14:56:28 +0200

    second commit

--- SAE_p1.sql
+++ SAE_p1.sql
@@ -33,3 +33,25 @@ create table s_activite as
 select * from ensinfo.S_ACTIVITE;
 create table s_participation as
 select * from ensinfo.S_PARTICIPATION;
+
+alter table s_club add primary key(numClub);
+alter table s_membre add primary key(codeMembre);
+alter table s_typemateriel add primary key(codeType);
+alter table s_adhesion add primary key(numAdhesion);
+alter table s_adhesion add foreign key(codeMembre) references s_membre(codeMembre);
+alter table s_materiel add primary key(idMateriel);
+alter table s_materiel add foreign key(codeType) references s_typemateriel(codeType);
+alter table s_location add primary key(numLoc);
+alter table s_location add foreign key(codeMembre) references s_membre(codeMembre);
+alter table s_location add foreign key(numClub) references s_club(numClub);
+alter table s_activite add primary key(numActivite);
+alter table s_activite add foreign key(numClub) references s_club(numClub);
+alter table s_stock add primary key(idMateriel, numClub);
+alter table s_stock add foreign key(idMateriel) references s_materiel(idMateriel);
+alter table s_stock add foreign key(numClub) references s_club(numClub);
+alter table s_detaillocation add primary key(numLoc, idMateriel);
+alter table s_detaillocation add foreign key(numLoc) references s_location(numLoc);
+alter table s_detaillocation add foreign key(idMateriel) references s_materiel(idMateriel);
+alter table s_participation add primary key(numActivite, codeMembre);
+alter table s_participation add foreign key(numActivite) references s_activite(numActivite);
+alter table s_participation add foreign key(codeMembre) references s_membre(codeMembre);
